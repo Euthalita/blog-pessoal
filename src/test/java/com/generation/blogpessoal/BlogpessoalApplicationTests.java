f1b8e59da57d21755c21ad397a74c9a7d1ed56de
Thalita <eutha.lita.as@gmail.com>
2024-12-13 20:16:34 -0300

    Deploy da aplicação

---
+++ src/test/java/com/generation/blogpessoal/BlogpessoalApplicationTests.java
@@ -0,0 +1,12 @@
+package com.generation.blogpessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
